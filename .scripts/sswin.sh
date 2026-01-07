7e61cd33bec46ca7e4339bc6e5a562071f039d46
Maciej Wieczor-Retman <m.wieczorretman@pm.me>
2026-01-07 20:54:41 +0100

    i3: Screenshot tools

---
+++ .scripts/sswin.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+##ssarea script
+DATE=$(date +%Y-%m-%d-%H:%M:%S)
+YEAR=$(date +%Y)
+MONTH=$(date +%m)
+DIRNAME="/home/maciej/Pictures/${YEAR}/${MONTH}"
+FILENAME="${DIRNAME}/Screenshot-${DATE}.png"
+
+## makes dir if it doesn't exist
+mkdir -p $DIRNAME
+## take screenshot
+maim -i $(xdotool getactivewindow) $FILENAME
+
+## copy file to clipboard
+xclip -selection clipboard -t image/png -i $FILENAME
